002993fc447bb8a0ac9a00672566602783fe6d85
bpd19 <biel.puig@estudiantat.upc.edu>
2023-05-29 23:14:40 +0200

    a

--- src/load_shoes.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-//it doesn't work
-const json = readFileSync('../shoes.json').toString();
-const shoes = JSON.parse(json);
-console.log(shoes);
-console.log(JSON.stringify(mensaje));
